f5239819f008efdfec27feb295b866989fd4675a
vanchrisjos <50887511+vanchrisjos@users.noreply.github.com>
2019-05-22 21:27:43 -0500

    fenix

---
+++ pagina/mecanico.php
@@ -0,0 +1,46 @@
+<?php 
+
+$conexion=mysqli_connect('bhvvnhldc187tsiitiio-mysql.services.clever-cloud.com','ub7skhfoscwcmgdz','eUcpD6hq3DsacPx04Q5o','bhvvnhldc187tsiitiio');
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mostrar datos</title>
+</head>
+<body>
+
+<br>
+
+	<table border="1" >
+		<tr>
+			<td>NOMBRE</td>
+			<td>TELEFONO</td>
+			<td>CORREO</td>
+			<td>PRECIO</td>
+			<td>UBICACION</td>	
+		</tr>
+
+		<?php 
+		$sql="SELECT * from mecanico";
+		$result=mysqli_query($conexion,$sql);
+
+		while($mostrar=mysqli_fetch_array($result)){
+		 ?>
+
+		<tr>
+			<td><?php echo $mostrar['NOMBRE'] ?></td>
+			<td><?php echo $mostrar['TELEFONO'] ?></td>
+			<td><?php echo $mostrar['CORREO'] ?></td>
+			<td><?php echo $mostrar['PRECIO'] ?></td>
+			<td><?php echo $mostrar['UBICACION'] ?></td>
+		
+		</tr>
+	<?php 
+	}
+	 ?>
+	</table>
+<body background="fenix2 (2).png">
+</body>
+</html>
